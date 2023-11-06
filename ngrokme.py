a63f766de043d5ac4000f0119361aec596677708
RobeSantoro <santoro.robe@gmail.com>
2023-11-06 20:48:25 +0100

    if dunder bla bla

--- ngrokme.py
+++ ngrokme.py
@@ -9,21 +9,26 @@ $ Start-Process "$env:windir\system32\mstsc.exe" -ArgumentList "/v:<public URL>"
 import json
 import subprocess
 
-# Query the ngrok API via command line
-cmd = 'ngrok api endpoints list'
-p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
-(output, err) = p.communicate()
-p_status = p.wait()
+def ngrokme():
+        
+    # Query the ngrok API via command line
+    cmd = 'ngrok api endpoints list'
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+    (output, err) = p.communicate()
+    p_status = p.wait()
 
-# Parse the JSON output
-data = json.loads(output)
-public_url = data['endpoints'][0]['public_url'][6:]
+    # Parse the JSON output
+    data = json.loads(output)
+    public_url = data['endpoints'][0]['public_url'][6:]
 
-print('Public URL: ')
-print(public_url)
+    print('Public URL: ')
+    print(public_url)
 
-# Start a RDP session to the public URL
-powershell_cmd = f'Start-Process "$env:windir\system32\mstsc.exe" -ArgumentList "/v:{ public_url }"'
+    # Start a RDP session to the public URL
+    powershell_cmd = f'Start-Process "$env:windir\system32\mstsc.exe" -ArgumentList "/v:{ public_url }"'
 
-# Execute the PowerShell command
-subprocess.run(["powershell", "-Command", powershell_cmd])
+    # Execute the PowerShell command
+    subprocess.run(["powershell", "-Command", powershell_cmd])
+
+if __name__ == '__main__':
+    ngrokme()
